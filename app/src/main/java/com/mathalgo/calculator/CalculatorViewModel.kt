e5371378de7c38a5a2b3e142d81327592bbf4dcb
Stephan Branczyk <stephan@branczyk.org>
2022-05-26 17:02:38 -0700

    added state for the calculator

---
+++ app/src/main/java/com/mathalgo/calculator/CalculatorViewModel.kt
@@ -0,0 +1,11 @@
+package com.mathalgo.calculator
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel: ViewModel() {
+    var state by mutableStateOf(CalculatorState())
+    private set
+}
